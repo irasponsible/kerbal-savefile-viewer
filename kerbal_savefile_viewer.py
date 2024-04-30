b1a2ee7acb30eccb71173a294bdd28bb581117a2
irasponsible <138816927+irasponsible@users.noreply.github.com>
2024-04-30 18:58:43 +1000

    Add Command Line Arguments and Referencing to ksv
    - Added command line arguments
    - Added method (and command line args) to refer to a specific object in the tree
    - Tidy up

--- kerbal_savefile_viewer.py
+++ kerbal_savefile_viewer.py
@@ -21,31 +21,27 @@ __version__ = 202404.28
 __author__ = 'IKJ'
 
 # --- Initialise ---
-from rich import print         # Formatting
-from datetime import datetime  # Formatting Datetime
-import json                    # JSON General Handling
-from itertools import groupby  # Used for JSON duplicate key handling
 
-# - Config
-debug_output_name = "Title"
-debug_output_time = "persistentTimestamp"
-debug_output_vers = "version"
+import argparse        # Argument Parsing
+import itertools       # Duplicate key handling
+import json            # JSON General Handling
+
+from rich import print         # Print Formatting
 
 
+# --- Functions ---
 # JSON Encoding - Duplicate Pairs
 def duplicate_object_pairs_hook(pairs):
-    """
-    Allows parsing a JSON with duplicate keys.
-    {"key":"value", "key" : "value2"} --> { "key" : ["value", "value2"] }
-    
-    https://stackoverflow.com/questions/29203165/dealing-with-json-with-duplicate-keys"""
+    ''' Allows parsing a JSON with duplicate keys.
+        {"key":"value", "key" : "value2"} --> { "key" : ["value", "value2"] }
+        https://stackoverflow.com/questions/29203165/dealing-with-json-with-duplicate-keys '''
 
     def _key(pair):
         (k, v) = pair
         return k
 
     def gpairs():
-        for (k, group) in groupby(pairs, _key):
+        for (k, group) in itertools.groupby(pairs, _key):
             ll = [v for (_, v) in group]
             (v, *extra) = ll
             yield (k, ll if extra else v)
@@ -53,13 +49,78 @@ def duplicate_object_pairs_hook(pairs):
     return dict(gpairs())
 
 
+# Split a string reference path into a list, and convert any numbers to integers
+def treepath_split(reference: str, branch: list = ["GAME"]) -> list[str, int]:
+    """Split a path string into a list that can be passed to dictionary_reference_from_list()"""
+    # Split the provided path on / and strip trailing
+    path = reference.rstrip('/').split('/')
+
+    # If they ask for everything, let 'em have it
+    if path[-1] == 'ALL':
+        return branch
+
+    # Convert any numbers into integer type
+    for count, step in enumerate(path):
+        try:
+            path[count] = int(step)
+        except ValueError:
+            pass
+
+    return branch + path
+
+
+# Find a specific item from a path given as a list
+def dictionary_reference_from_list(data, keys: list):
+    """Recursive function. Return a value from a dictionary using a path given as a list."""
+    if keys:
+        return dictionary_reference_from_list(data[keys[0]], keys[1:])
+    else:
+        return data
+
+
+# --- Configuration ---
+# - Command Line Arguments
+argp = argparse.ArgumentParser(prog="kerbal_sv",
+                                description="Convert a KSP save file to JSON",
+                                epilog="Please note, using -q ALL or -r ALL may lag or crash, because they are very long.")
+argp.add_argument('filepath', nargs='+', type=argparse.FileType('r'))
+argp.add_argument('-d', metavar='[depth]', action='store', help='change max depth of the input .sfs file', type=int)
+argp.add_argument('-f', '--format', action='store_const', help='format the .json output with indenting and newlines', const=2)
+argp.add_argument('-k', '--kspv', action='store_const', help='show the full ksp version', const="versionFull")
+argp.add_argument('-t', '--testing', action='store_true', help='output intermediate step to text file (for debugging)')
+
+printouts = argp.add_argument_group('printouts', 'print an object. use / to get deeper into the tree, and integers for duplicate names.')
+printouts.add_argument('-p', action='append', metavar='[ref/to/obj]', help='print an object inside "PARAMETERS", or ALL')
+printouts.add_argument('-q', action='append', metavar='[ref/to/obj]', help='print an object inside "GAME", or ALL')
+printouts.add_argument('-r', action='append', metavar='[ref/to/obj]', help='print an object inside "FLIGHTSTATE", or ALL')
+
+# - Defaults
+argp.set_defaults(d=16, format=False, p=[], q=[], r=[], kspv='version', testing=False)
+output_name = 'Title'
+output_time = 'persistentTimestamp'
+
+# - Parsing
+args = argp.parse_args()
+filepath = args.filepath
+
+# Assemble a list of paths within the sfs that the user wants to have printed to the screen.
+printout_paths = []
+for item in args.p:
+    printout_paths.append(treepath_split(item, ["GAME", "PARAMETERS"]))
+
+for item in args.q:
+    printout_paths.append(treepath_split(item, ["GAME"]))
+
+for item in args.r:
+    printout_paths.append(treepath_split(item, ["GAME", "FLIGHTSTATE"]))
+
+print(args)
+print(printout_paths)
+
 # --- File Load ---
-filepath = '/home/ikj/Projects/python/data/persistent.sfs'
-with open(filepath, 'r') as file:
-    sfs = file.read()
+sfs = args.filepath[0].read()
 
 # --- Convert to JSON formatting ---
-
 # Clean Input String of tabs and whitspace at the end of lines
 sfs = sfs.replace("\t", '').replace(" \n", '\n').replace('\n ', '\n')
 
@@ -75,28 +136,37 @@ sfs = sfs.replace('\n', '",\n"')       # Add double-quotes at the start and end 
 sfs = sfs.replace(':{",', '":{')       # Fix misquoted objects and braces caused by the above
 sfs = sfs.replace(':{}"', '":{}')      #     ''
 
-for n in range(16):            # Fix Dedenting Braces
-    i = 10 - n                 # I really hope nothing ever goes this deep. Deepest I can see is 8 or so.
-    fr = ',\n"' + ('}' * i)    # "}}}}} with i braces
-    to = ('}' * i) + ',\n"'    # }}}}}" with i braces
+for n in range(args.d):        # Fix Dedenting Braces
+    i = args.d - n             # Deepest I can see is 8 or so. default 16 should be plenty
+    fr = ',\n"' + ('}' * i)    #    "}}}}} with <i> braces
+    to = ('}' * i) + ',\n"'    #    }}}}}" with <i> braces
     sfs = sfs.replace(fr, to)
 
 # Fix start and end of file to make sure we have valid JSON
 sfs = '{"' + sfs[:-3] + '}'
 
 # --- Output ---
-# - Output munged string to TXT file for Debugging
-with open(filepath + '.debug.json', 'w') as file:
-    file.write(sfs)
+# - Debug - Output munged string to TXT file for Debugging
+if args.testing:
+    with open(filepath + '.debug.json', 'w') as file:
+        file.write(sfs)
 
 # - Read string as JSON
 jfs = json.loads(sfs, object_pairs_hook=duplicate_object_pairs_hook)
 # Printout to confirm it worked
 
-print(f'\n------\n [bold]{jfs["GAME"][debug_output_name]}[/bold]\n------\n')
-print("Save Time:\t", jfs["GAME"][debug_output_time][0:16].replace("T", " "))
-print("KSP Version:\t", jfs["GAME"][debug_output_vers], "\n")
+print(f'[bold]{jfs["GAME"][output_name]}[/bold]\n')
+print("Save Time:\t", jfs["GAME"][output_time][0:16].replace("T", " "))
+print("KSP Version:\t", jfs["GAME"][args.kspv], "\n")
+
+# Printing a specific item
+for reference in printout_paths:
+    print(reference)
+    try:
+        print(dictionary_reference_from_list(jfs, reference))
+    except KeyError as k_err:
+        print("Reference Failed, please check the spelling. This is case sensitive!")
 
 # - Output to JSON file
-with open(filepath + '.json', 'w') as file:
-    file.write(json.dumps(jfs, indent=2))
+with open(filepath[0].name + '.json', 'w') as file:
+    file.write(json.dumps(jfs, indent=args.format))
